80b217b496a4523ae1ae92acca0fb44b9aee9a6f
Kelli Shaver <kelli@kellishaver.com>
2017-02-20 21:46:38 -0500

    remove unused javascript

--- views/footer.php
+++ views/footer.php
@@ -4,7 +4,4 @@
       <a href="./history.php">History</a> | <a href="./settings.php">Settings</a> | <a href="./session.php">Log Out</a>
     <?php endif ?>
   </footer>
-
-  <script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
-  <script type="text/javascript" src="./js/app.js"></script>
 </body>
